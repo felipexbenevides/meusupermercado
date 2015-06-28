8834dea3b3620773025b16a0fcb867172e3159bf
Raphael Mendes <raphaelmendes18@gmail.com>
2015-06-28 11:46:42 -0300

    email fail

--- meusupermercadoApp/client/meusupermercadoApp.js
+++ meusupermercadoApp/client/meusupermercadoApp.js
@@ -18,6 +18,6 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    process.env.MAIL_URL = 'smtp://postmaster%40meteorize.mailgun.org:YOURPASSWORD@smtp.mailgun.org:587';
   });
 }
