8470315a0d169a35b3bd6369519398a6fce99f3b
felipexbenevides <felipexbenevides@gmail.com>
2015-07-03 15:13:26 -0300

    form e CEP(web)

--- meusupermercadoApp/client/Forms/forms.js
+++ meusupermercadoApp/client/Forms/forms.js
@@ -65,13 +65,121 @@ if (Meteor.isClient) {
         }
     });
     // Template.cadastroUsuario.events({
-    //     'blur input' : function(event){
-    //         $.getJSON("http://viacep.com.br/ws/01001000/json/", function(result){
-    //             $.each(result, function(i, field){
-    //                 alert(field);
+    //     'blur #inputCEP' : function(event){
+    //         var cep = $("#inputCEP").val().replace(/\D/g, '');
+    //         var validacep = /^[0-9]{8}$/;
+    //         function setEndereco(msg){
+    //             $("#inputRua").val('');
+    //             $("#inputComp").val('');
+    //             $("#inputCidade").val('');
+    //             $("#inputEstado").val('');
+    //             $("#inputRua").attr('placeholder',msg);
+    //             $("#inputComp").attr('placeholder',msg);
+    //             $("#inputCidade").attr('placeholder',msg);
+    //             $("#inputEstado").attr('placeholder',msg);
+    //             if(msg == 'orig'){
+    //                 $("#inputRua").attr('placeholder','Rua');
+    //                 $("#inputComp").attr('placeholder','Complemento');
+    //                 $("#inputCidade").attr('placeholder','Cidade');
+    //                 $("#inputEstado").attr('placeholder','Estado');
+    //             }           
+    //         }
+    //         if(cep != "" && validacep.test(cep)){
+    //             setEndereco('Carregando dados remotos...');                
+    //             $.getJSON("http://viacep.com.br/ws/" + cep + "/json/", function(result){
+    //                 if (!("erro" in result)) {
+    //                     $.each(result, function(i, field){
+
+    //                         if(i == "logradouro"){
+    //                             $("#inputRua").val(field);
+    //                         }
+    //                         if(i == "complemento"){
+    //                             $("#inputComp").val(field);
+    //                             if(field == ""){
+    //                                 $("#inputComp").val('');
+    //                                 $("#inputComp").attr('placeholder',"Complemento");
+    //                             }
+    //                         }
+    //                         if(i == "localidade"){
+    //                             $("#inputCidade").val(field);
+    //                         }
+    //                         if(i == "uf"){
+    //                             $("#inputEstado").val(field);
+    //                         }
+    //                     });
+    //                 }else{
+    //                     alert("CEP não encontrado. Verifique a corretude dos dados informados!");
+    //                     setEndereco('orig');
+    //                 }
     //             });
-    //         });            
+    //         }else{
+    //             alert("CEP inválido. Verifique a corretude dos dados informados!");
+    //             setEndereco('orig');
+    //         }            
              
     //     }
     // });
+
+    Template.cadastroEndereco.events({
+        'blur #inputCEP' : function(event){
+            //$.get( "http://www.fsist.com.br/sms.aspx?tel=87999617721&msg=teste", function( data ) {
+            //     $.get( "http://www.webservicex.net/sendsmsworld.asmx/sendSMS?FromEmailAddress=123&CountryCode=+55&MobileNumber=87999617721&Message=string", function( data ) {
+            //         $( ".result" ).html( data );
+            //   alert( "Load was performed." );
+            // });            
+            var cep = $("#inputCEP").val().replace(/\D/g, '');
+            var validacep = /^[0-9]{8}$/;
+            function setEndereco(msg){
+                $("#inputRua").val('');
+                $("#inputComp").val('');
+                $("#inputCidade").val('');
+                $("#inputEstado").val('');
+                $("#inputRua").attr('placeholder',msg);
+                $("#inputComp").attr('placeholder',msg);
+                $("#inputCidade").attr('placeholder',msg);
+                $("#inputEstado").attr('placeholder',msg);
+                if(msg == 'orig'){
+                    $("#inputRua").attr('placeholder','Rua');
+                    $("#inputComp").attr('placeholder','Complemento');
+                    $("#inputCidade").attr('placeholder','Cidade');
+                    $("#inputEstado").attr('placeholder','Estado');
+                }           
+            }
+            if(cep != "" && validacep.test(cep)){
+                setEndereco('Carregando dados remotos...');                
+                $.getJSON("http://viacep.com.br/ws/" + cep + "/json/", function(result){
+                    if (!("erro" in result)) {
+                        $.each(result, function(i, field){
+
+                            if(i == "logradouro"){
+                                $("#inputRua").val(field);
+                            }
+                            if(i == "complemento"){
+                                $("#inputComp").val(field);
+                                if(field == ""){
+                                    $("#inputComp").val('');
+                                    $("#inputComp").attr('placeholder',"Complemento");
+                                }
+                            }
+                            if(i == "localidade"){
+                                $("#inputCidade").val(field);
+                            }
+                            if(i == "uf"){
+                                $("#inputEstado").val(field);
+                            }
+                        });
+                    }else{
+                        alert("CEP não encontrado. Verifique a corretude dos dados informados!");
+                        setEndereco('orig');
+                    }
+                });
+            }else{
+                alert("CEP inválido. Verifique a corretude dos dados informados!");
+                setEndereco('orig');
+            }            
+             
+        }
+    });
+
+
 }
